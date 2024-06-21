e0a831b5214324fd1569253627de4148fe236f87
Gigher <gastelo1301@gmail.com>
2024-06-21 09:23:14 +0300

    add show feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {
   Admin,
-  Resource
+  Resource,
+  ShowGuesser
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
@@ -10,11 +11,13 @@ import { UserCreate } from "./components/UserCreate";
 import { PostList } from "./components/PostList";
 import { PostEdit } from "./components/PostEdit";
 import { PostCreate } from "./components/PostCreate";
+import { UserShow } from "./components/UserShow";
+import { PostShow } from "./components/PostShow";
 
 
 export const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
-    <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate}/>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
+    <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} show={UserShow} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} show={PostShow} />
   </Admin>
 );
